ee19d1e41c250a7ed02d45bff203f2fcc0bf4c4e
360ied <19516527+360ied@users.noreply.github.com>
2022-03-11 10:03:19 -0500

    use terminal in raw mode

--- cmd/pfcalcli/main.go
+++ cmd/pfcalcli/main.go
@@ -6,32 +6,108 @@ import (
 	"strconv"
 	"strings"
 
+	"pfcalcli/internal/intmath"
 	"pfcalcli/internal/stackutil"
 	"pfcalcli/pkg/libpfcalc"
+
+	"golang.org/x/term"
 )
 
 var build = "DEVELOPMENT_BUILD"
 
+func moveLeft() {
+	_, _ = os.Stdout.WriteString("\u001b[1000D") // move all the way left
+}
+
+func moveCursor(pos int) {
+	_, _ = os.Stdout.WriteString("\u001b[" + strconv.Itoa(pos) + "C")
+}
+
 func main() {
+	_, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+
 	_, _ = os.Stdout.WriteString("pfcalcli build " + build + "\n")
+	moveLeft()
 
-	var stack []float64
+	var (
+		stack []float64
+		// history []string
+	)
 
-	scanner := bufio.NewScanner(os.Stdin)
+	r := bufio.NewReader(os.Stdin)
 
 	for {
-		_, _ = os.Stdout.WriteString(strconv.Itoa(len(stack)) + "> ")
+		prompt := strconv.Itoa(len(stack)) + "> "
+		_, _ = os.Stdout.WriteString(prompt)
 
-		if !scanner.Scan() {
-			break
+		input := ""
+		index := 0
+		// read input
+		for {
+			c, err := r.ReadByte()
+			if err != nil {
+				panic(err)
+			}
+
+			if c == 3 {
+				// ctrl+c
+				moveLeft()
+				_, _ = os.Stdout.WriteString("\nExiting...\n")
+				moveLeft()
+				os.Exit(0)
+			} else if c >= 32 && c <= 126 {
+				// printable character
+				input = input[:index] + string(c) + input[index:]
+				index++
+			} else if c == 10 || c == 13 { // CRLF
+				break
+			} else if c == 27 { // ESC
+				next1, err := r.ReadByte()
+				if err != nil {
+					panic(err)
+				}
+
+				next2, err := r.ReadByte()
+				if err != nil {
+					panic(err)
+				}
+
+				if next1 == 91 {
+					if next2 == 68 { // left
+						index = intmath.Max(0, index-1)
+					} else if next2 == 67 { // right
+						index = intmath.Min(len(input), index+1)
+					}
+				}
+			} else if c == 127 { // backspace
+				if index > 0 {
+					input = input[:index-1] + input[index:]
+					index--
+				}
+			}
+
+			// TODO: make this not call write so many times
+			moveLeft()
+			_, _ = os.Stdout.WriteString("\u001b[0K")
+			_, _ = os.Stdout.WriteString(prompt + input)
+			moveLeft()
+
+			if index > 0 {
+				moveCursor(index + len(prompt))
+			}
 		}
 
-		expressionStr := scanner.Text()
+		_, _ = os.Stdout.WriteString("\n")
+		moveLeft()
 
 		var err error
-		stack, err = libpfcalc.Evaluate(stack, expressionStr)
+		stack, err = libpfcalc.Evaluate(stack, input)
 		if err != nil {
 			_, _ = os.Stdout.WriteString("evaluate: " + err.Error() + "\n")
+			moveLeft()
 		}
 
 		_, top, found := stackutil.Pop(stack)
@@ -41,10 +117,6 @@ func main() {
 		} else {
 			_, _ = os.Stdout.WriteString("_\n")
 		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		_, _ = os.Stdout.WriteString("main: scanner error: " + err.Error() + "\n")
-		os.Exit(1)
+		moveLeft()
 	}
 }

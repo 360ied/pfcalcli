8d595b9013073677783e7aa7b14b5e3afa2ae814
360ied <19516527+360ied@users.noreply.github.com>
2022-04-05 11:28:47 -0400

    reformat build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -x
 
 rm build/pfcalcli
 
-go build -o build/pfcalcli -ldflags "-X main.build=$(date -uIminutes) -w -s" pfcalcli/cmd/pfcalcli &&
+go build -o build/pfcalcli -ldflags \
+	"-X main.build=$(date -uIminutes) -w -s" \
+	pfcalcli/cmd/pfcalcli &&
 	upx --ultra-brute build/pfcalcli

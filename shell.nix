85cb9451fd262ca940059f400aacce4f27e331ba
360ied <19516527+360ied@users.noreply.github.com>
2022-04-17 12:03:59 -0400

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,8 @@
+let
+  pkgs = import (builtins.fetchGit {
+    name = "nixos-unstable-2022-04-17";
+    url = "https://github.com/nixos/nixpkgs";
+    ref = "refs/heads/nixos-unstable";
+    rev = "75ad56bdc927f3a9f9e05e3c3614c4c1fcd99fcb";
+  }) { };
+in pkgs.mkShell { buildInputs = [ pkgs.go pkgs.gotools ]; }
